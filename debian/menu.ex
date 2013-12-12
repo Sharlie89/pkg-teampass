77b3abcb434a8c03a5bfa8fceea32d6da463d1ea
Carlos Álvarez Barba (Sharlie) <sharlie89@gmail.com>
2013-12-12 13:13:56 +0100

    Remove unused example files

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(teampass):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="teampass" command="/usr/bin/teampass"
--- debian/teampass.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the teampass package
-#
-0 4	* * *	root	[ -x /usr/bin/teampass_maintenance ] && /usr/bin/teampass_maintenance
